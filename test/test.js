f3e25fc92156feef54ad6dacd8c6d06af8c9cf9c
23watejona <58493718+23watejona@users.noreply.github.com>
2023-06-01 00:20:18 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ import fs from 'fs'
 import assert from 'assert'
 
 let generatedGrammar = index.generateGrammar(fs.readFileSync("./test/testcommandlist.json"), fs.readFileSync("./test/testfunctionlist.json"))
-fs.writeFileSync('./test/output.txt', generatedGrammar)
 assert(generatedGrammar == fs.readFileSync('./test/commandGrammar.pegjs').toString(), "Generated Command Grammar does not match expected Command Grammar")
 index.setGrammar(generatedGrammar)
 let indentParsed = indentParser.parse(fs.readFileSync('./test/script.txt').toString())
