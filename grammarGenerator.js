f8a36c54ec4d292754a72e9e81723d44ec7dec56
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-07-05 17:55:37 -0400

    Use map instead of for loop

--- grammarGenerator.js
+++ grammarGenerator.js
@@ -3,8 +3,8 @@
 //Also takes in a list of all the functions we want to be able to parse for
 export function generateGrammar(commandList, functionList) {
 	//generate the grammar from the template
-    let generatedGrammar = 
-`file = first:(function/javafunction)+
+	let generatedGrammar =
+		`file = first:(function/javafunction)+
 {
 	return first
 }
@@ -40,24 +40,24 @@ command = first:(${Object.keys(commandList).join("/")}) t:timeout?
 {
 	return first + (t != null ? t : "")
 }
-${(()=>{
-	let res = ""
-	for (let i in commandList) {
-		res += createCommandToken(i, commandList[i])
-	}
-	return res
-})()
-}f'function' = first:(${Object.keys(functionList).join("/")}) t:timeout?
+${(() => {
+			let res = ""
+			for (let i in commandList) {
+				res += createCommandToken(i, commandList[i])
+			}
+			return res
+		})()
+		}f'function' = first:(${Object.keys(functionList).join("/")}) t:timeout?
 {
 	return first + (t != null ? t : "")
 }
-${(()=>{
-	let res =""
-	for (let i in functionList) {
-		res += createFunctionToken(i, functionList[i])
-	}
-	return res
-})()}timeout = space "with timeout" space p:text
+${(() => {
+			let res = ""
+			for (let i in functionList) {
+				res += createFunctionToken(i, functionList[i])
+			}
+			return res
+		})()}timeout = space "with timeout" space p:text
 {
 	return ".withTimeout(" + p + ")"
 }
@@ -76,31 +76,27 @@ textnospace = s:[a-zA-Z0-9._-]* {return s.join("")}
 bracketed = "{" s:[^}]* "}" {return s.join("")}
 text = s:[^\\n]+ {return s.join("")}
 newline'new line' = "\\n" { return ""}`
-    return generatedGrammar
+	return generatedGrammar
 }
 
-function createCommandToken(commandFullName, commandObject){
-	
+function createCommandToken(commandFullName, commandObject) {
+
 	//generate a command token based on the template
 	let scriptingName = commandObject.name
 	let parameters = commandObject.parameters
 
-	let firstLineParameters = []
-	let returnParameters = []
 	// For each parameter, create a indexed label, starting at one
-	for (let index = 1; index < parameters.length + 1; index++) {
-		//format = p1:p etc
-		firstLineParameters.push(`p${index}:p`)
-		//format ends up as +p... for each
-		returnParameters.push(`+p${index}`)
-	}
+	//format = p1:p etc
+	let firstLineParameters = parameters.map((param, index) => `p${index + 1}:p`)
+	//format ends up as +p... for each
+	let returnParameters = parameters.map((param, index) => `+p${index + 1}`)
 
 	//convert to text and change the delimiters between each of these items from commas to spaces
 	let firstLineParametersFormatted = firstLineParameters.join(" ")
-	
+
 	//convert to text and change the delimiters from , to +","
 	let returnParametersFormatted = returnParameters.join('+","')
-	
+
 	let commandToken = `${commandFullName} = newline "${scriptingName}" ${firstLineParametersFormatted}
 {
 	return "new ${commandFullName}("${returnParametersFormatted}+")"
@@ -109,8 +105,8 @@ function createCommandToken(commandFullName, commandObject){
 	return commandToken
 }
 
-function createFunctionToken(functionFullName, functionScriptingName){
-		//generate a function token based on the template
+function createFunctionToken(functionFullName, functionScriptingName) {
+	//generate a function token based on the template
 	return `${functionFullName} = newline "${functionScriptingName}"
 {
 	return "${functionFullName}()"
